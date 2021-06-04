5f58c0eef75ae05e1ef47942062f005c39ff8980
Robert Attard <robert.attard@indexechange.com>
2021-06-04 11:21:01 -0400

    config parameter for gleam path

--- lib/mix/tasks/compiler/gleam.ex
+++ lib/mix/tasks/compiler/gleam.ex
@@ -2,7 +2,9 @@ defmodule Mix.Tasks.Compile.Gleam do
   use Mix.Task.Compiler
 
   def run(_args) do
-    case Mix.shell().cmd("gleam build .") do
+    gleam_path = Application.get_env(:mix_gleam, :gleam_path, "gleam")
+
+    case Mix.shell().cmd(gleam_path <> " build .") do
       0 -> {:ok, []}
       status -> exit(status)
     end
